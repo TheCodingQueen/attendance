e0a964f57209671ccc4c46676a362b0744f29d10
Rashi <rasraw003@gmail.com>
2019-03-16 12:40:32 +0530

    Button is working for update but  some error exist

--- src/com/attendance/EditableTableExample.java
+++ src/com/attendance/EditableTableExample.java
@@ -3,6 +3,7 @@ import com.attendance.misc.dbConnect;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -16,14 +17,16 @@ import java.text.SimpleDateFormat;
 
 public class EditableTableExample extends JFrame {
 
-    public EditableTableExample() {
-        int VclassID;
-        int VStudentID;
-        String VAtt1 = "";
-        String VAtt2 = "";
-        Date VAttDate = null;
+    int vClassId;
+    int VStudentID;
+    String VAtt1 = "";
+    String VAtt2 = "";
+    Date VAttDate = null;
 
-        ArrayList<Attendance> al = new ArrayList<>();
+    ArrayList<Attendance> al = new ArrayList<>();
+
+
+    public EditableTableExample() {
         String query = "select a.idstudent,a.classid,b.Name,a.Att1,a.Att2 from attendance.Attendance a, attendance.Student b where a.idstudent = b.idstudent;";
 
         Connection con = dbConnect.getConnection();
@@ -67,64 +70,48 @@ public class EditableTableExample extends JFrame {
         b.setVisible(true);
         table.setBounds(30,60,500,170);
 
+        int totrec = al.size();
+        // create the java mysql update preparedstatement
+        String queryUpdate = "update attendance.Attendance set Att1 = ?, Att2 = ? where classID = ? and studentID = ? and date = ?";
 
-       /* b.addActionListener(new ActionListener() {
+
+        b.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                int totrec = al.size();
-                // create the java mysql update preparedstatement
-//                String query = "update attendance.Attendance set Att1 = ?, Att2 = ? where classID = ? and studentID = ? and date = ?";
-//                PreparedStatement preparedStmt = conn.prepareStatement(query);
+                try {
+                    PreparedStatement preparedStmt = con.prepareStatement(queryUpdate);
 
-                for (int i = 0; i < totrec; i++) {
-                    System.out.println(model1.getValueAt(i, 4) + " " + model1.getValueAt(i, 5));
+                    for (int i = 0; i < totrec; i++) {
+                        System.out.println(model1.getValueAt(i, 4) + " " + model1.getValueAt(i, 5));
 
-                    if (Boolean.valueOf(model1.getValueAt(i, 1)) = true) {
-                        VAtt1 = "Y";
-                    }
+                        if (((Boolean) model1.getValueAt(i, 4)).booleanValue())
+                            {
+                                preparedStmt.setString(1, "Y");
+                            }
 
 
-                    if (Boolean.valueOf(model1.getValueAt(i, 5)) = true) {
-                        VAtt2 = "Y";
-                    }
+                        if (((Boolean) model1.getValueAt(i, 5)).booleanValue())
+                            {
+                                preparedStmt.setString(2, "Y");
 
-                    VclassID = model1.getValueAt(i, 1);
-                    VStudentID = model1.getValueAt(i, 0);
+                            }
+
+                    /*vClassId = (int)model1.getValueAt(i, 1);
+                    VStudentID = (int)model1.getValueAt(i, 0);*/
 //                    VAttDate = ;
 
-                    ps.setString(1, VAtt1);
-                    ps.setString(2, VAtt2);
-                    ps.setInt(3, VclassID);
-                    ps.setInt(4, VStudentID);
-                    ps.setInt(5, VAttDate);
+//                    preparedStmt.setString(1, VAtt1);
+//                    preparedStmt.setString(2, VAtt2);
 
-                    /*  need two String variables VAtt1, VAtt2 to hold value for Att1 & Att2
-                        get value of model1.getValueAt(i,3) & model1.getValueAt(i,4)
-                        get value of classID model1.getValueAt(i,0)  into a variable VclassId
-                        get value of studentID model1.getValueAt(i,1)  into a variable VstudentID
-
-                       if model1.getValueAt(i,3) = TRUE then
-                       {VAtt1="y";}
-
-                       if model1.getValueAt(i,4) = TRUE then
-                       {VAtt2="y";}
-
-                       VclassID = model1.getValueAt(i,1);
-                       VStudent =model1.getValueAt(i,0);
-                       VAttDate = This is date for which Attendance is being captured and this must be captured in UI from the user into a variable;
-                        then set value of VAtt1 ='Y' VAtt2='Y'
-
-                        preparedStmt.setString(1, VAtt1);
-                        preparedStmt.setString(2, VAtt2);
-                        preparedStmt.setInt   (3, VclassID);
-                        preparedStmt.setInt   (4, vstudentID);
-                        preparedStmt.setInt   (5, VAttDate);
-                      // execute the java preparedstatement
-                      preparedStmt.executeUpdate();
-
-                }
+                            preparedStmt.setInt(3, (int) model1.getValueAt(i, 1));
+                            preparedStmt.setInt(4, (int) model1.getValueAt(i, 0));
+                            preparedStmt.setDate(5, new java.sql.Date(VAttDate.getTime()));
+                            // execute the java preparedstatement
+                            preparedStmt.executeUpdate();
+                    }
+                } catch (Exception e1) {e1.printStackTrace(); }
             }
         });
-*/
+
 
     }
 
